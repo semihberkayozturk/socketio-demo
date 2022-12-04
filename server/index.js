132ca09ee8dfb714381d3d2248aa4473b5248b79
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-12-04 14:47:57 +0300

    add some functionalities

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const path = require("path");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const server = app.listen(8080, () => console.log("Server is listening on port 8080!"))
-
-const io = require("socket.io")(server, {
-    cors: { origin: "*" }
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../app/index.html"))
 });
 
 io.on("connection", (socket) => {
-    console.log("A user connected!");
-
-    socket.on("message", (message) => {
-        console.log(message)
-        io.emit("message", `${socket.id.substr(0,2)} said: ${message}`)
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", `${socket.id.substr(0,2)}: ${msg}`)
     })
+    socket.broadcast.emit("chat message", "A user joined the chat!")
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("chat message", "A user left the chat!")
+    })
+});
+
+server.listen(3000, () => {
+    console.log("Server listening on port 3000!");
 });
